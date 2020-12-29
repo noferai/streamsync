b87db33e257b05ba74a01a45461fe95f4b878b6a
Avbey <azovsky777@gmail.com>
2020-12-29 11:29:52 +0300

    Backend updated [1]

--- server/main.go
+++ server/main.go
@@ -1,104 +1,29 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"runtime"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "server/docs"
 
-	"github.com/gin-contrib/gzip"
-	"github.com/joho/godotenv"
-	"server/controllers"
-	"server/db"
-	"server/middlewares"
-
-	"github.com/gin-gonic/gin"
+	"server/api"
+	"server/pkg/config"
+	"server/pkg/database"
 )
 
-var auth = new(controllers.AuthController)
-
-//JWT Authentication middleware attached to each request that needs to be authenticated to validate the access_token in the header
-func TokenAuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		auth.TokenValid(c)
-		c.Next()
-	}
+func init() {
+	config.Setup()
+	database.Setup()
 }
 
+// @title StreamSync API
+// @version 1.0
+// @BasePath /api/v1
 func main() {
+	configuration := config.GetConfig()
 
-	ctx := context.TODO()
+	db := database.GetDB()
+	r := api.Setup(db)
 
-	//Start the default gin server
-	r := gin.Default()
-
-	//Load the .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file, please create one in the root directory")
-	}
-
-	r.Use(middlewares.CORSMiddleware())
-	r.Use(middlewares.RequestIDMiddleware())
-	r.Use(gzip.Gzip(gzip.DefaultCompression))
-
-	//Start PostgreSQL database
-	db.Init()
-
-	//Start Redis on database 1 - it's used to store the JWT but you can use it for anything else
-	db.InitRedis("1")
-
-	v1 := r.Group("/v1")
-	{
-		user := new(controllers.UserController)
-		v1.POST("/user/login", user.Login)
-		v1.POST("/user/register", user.Register)
-		v1.GET("/user/logout", user.Logout)
-
-		auth := new(controllers.AuthController)
-		//Rerfresh the token when needed to generate new access_token and refresh_token for the user
-		v1.POST("/token/refresh", auth.Refresh)
-
-	}
-
-	r.LoadHTMLGlob("./public/html/*")
-
-	r.Static("/public", "./public")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"appVersion": "v0.01",
-			"goVersion":  runtime.Version(),
-		})
-	})
-
-	r.NoRoute(func(c *gin.Context) {
-		c.HTML(404, "404.html", gin.H{})
-	})
-
-	fmt.Println("SSL", os.Getenv("SSL"))
-	port := os.Getenv("PORT")
-
-	if os.Getenv("ENV") == "PRODUCTION" {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
-	if os.Getenv("SSL") == "TRUE" {
-
-		SSLKeys := &struct {
-			CERT string
-			KEY  string
-		}{}
-
-		//Generated using sh generate-certificate.sh
-		SSLKeys.CERT = "./cert/myCA.cer"
-		SSLKeys.KEY = "./cert/myCA.key"
-
-		r.RunTLS(":"+port, SSLKeys.CERT, SSLKeys.KEY)
-	} else {
-		r.Run(":" + port)
-	}
-
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run("127.0.0.1:" + configuration.Server.Port)
 }
